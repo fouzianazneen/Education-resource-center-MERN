7481ad8126325bebd0917780b73e59643f18fb97
Fouzia nazneen <155993032+fouzianazneen@users.noreply.github.com>
2024-10-10 19:40:20 +0530

    Update api.jsx

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://education-resource-center-mern.onrender.com',
   withCredentials: true, // Send cookies along with requests
 });
 
