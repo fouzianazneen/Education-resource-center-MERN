ea385b947628fcce370f89d7ce2c4f3f43a966a4
Fouzia nazneen <155993032+fouzianazneen@users.noreply.github.com>
2024-10-10 19:00:54 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
-const Educator = require('../models/educator');
-const Student = require('../models/student');
+const Educator = require('../models/Educator');
+const Student = require('../models/Student');
 const router = express.Router();
 
 
